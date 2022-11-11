4372381d83330b58eb6ac7cf33fac1da24aeedaf
Dominic Soh <dominic.soh@mercari.com>
2022-11-11 22:15:53 +0900

    feat: items page

--- components/ListingItem.vue
+++ components/ListingItem.vue
@@ -1,23 +1,29 @@
 <template>
   <v-container>
-    <v-card>
-      <v-img
-        ref="cat"
-        :src="image"
-      />
-      <template #title>
-        {{ name }}
-      </template>
-      <template #subtitle>
-        {{ category }}
-      </template>
-    </v-card>
+    <nuxt-link
+      :to="id"
+      class="custom-link"
+    >
+      <v-card>
+        <v-img
+          ref="cat"
+          :src="image"
+        />
+        <template #title>
+          {{ name }}
+        </template>
+        <template #subtitle>
+          {{ category }}
+        </template>
+      </v-card>
+    </nuxt-link>
   </v-container>
 </template>
 
 <script lang="ts">
 export default defineComponent({
     props: {
+        id: {type: String, required: true},
         name: { type: String, required: true},
         category: { type: String, required: true},
         image: { type: String, required: true}
